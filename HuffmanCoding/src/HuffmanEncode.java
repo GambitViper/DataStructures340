0494f7e890c6ab10008bc746cd81bbe65e452280
GambitViper <baklund.zachary@uwlax.edu>
2017-10-16 19:12:04 -0500

    Progress made on HuffmanEncode and HuffmanTree plus test file

--- HuffmanCoding/src/HuffmanEncode.java
+++ HuffmanCoding/src/HuffmanEncode.java
@@ -10,18 +10,84 @@ import java.io.*;
 import java.util.*;
 
 public class HuffmanEncode {
-	
-	/**
-	 * Implements the huffman encoding algorithm
-	 * private methods as needed
-	 * @param in
-	 * @param out
-	 */
-	public HuffmanEncode(String in, String out){
-		
+
+	private PriorityQueue<Item> queue;
+	private int totalChars;
+
+	private class Item implements Comparable<Item> {
+		private int freq;
+		private HuffmanTree data;
+
+		public Item(HuffmanTree d, int f) {
+			freq = f;
+			data = d;
+		}
+
+		public int compareTo(Item o) {
+			return this.freq - o.freq;
+		}
+
+		public String toString() {
+			return data.toString();
+		}
+
 	}
 	
-	public static void main(String args[]){
+	private void buildTree(){
+		while(queue.size() > 1){
+			Item left = queue.poll();
+			Item right = queue.poll();
+			
+			HuffmanTree merge = new HuffmanTree(left.data, right.data, (char) 128);
+			
+			Item putBack = new Item(merge, left.freq + right.freq);
+			queue.add(putBack);
+		}
+	}
+
+	private void readFile(String filename) {
+		int[] frequencies = new int[128];
+		try {
+			FileReader fr = new FileReader(filename);
+			BufferedReader br = new BufferedReader(fr);
+			int currChar = br.read();
+			while (currChar != -1) {
+				if (currChar < 128) {
+					frequencies[currChar]++;
+					totalChars++;
+					currChar = br.read();
+				} else {
+					currChar = br.read();
+				}
+			}
+			for (int i = 0; i < frequencies.length; i++) {
+				if (frequencies[i] != 0) {
+					queue.add(new Item(new HuffmanTree((char) i), frequencies[i]));
+				}
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String args[]) {
 		new HuffmanEncode(args[0], args[1]);
+	}
+	
+	/**
+	 * Implements the huffman encoding algorithm private methods as needed
+	 * 
+	 * @param in
+	 *            filename of input
+	 * @param out
+	 *            filename of output
+	 */
+	public HuffmanEncode(String in, String out) {
+		queue = new PriorityQueue<Item>();
+		readFile(in);
+		buildTree();
+		System.out.println(queue);
+
 	}
 }
--- HuffmanCoding/src/HuffmanTree.java
+++ HuffmanCoding/src/HuffmanTree.java
@@ -7,121 +7,151 @@
  *
  */
 import java.util.*;
+
 public class HuffmanTree {
-	
-	private class Node{
+
+	private class Node {
 		private Node left;
 		private char data;
 		private Node right;
-		
-		private Node(Node l, char d, Node r){
+
+		private Node(Node l, char d, Node r) {
 			left = l;
 			data = d;
 			right = r;
 		}
 	}
-	
+
 	private Node root;
 	private Node current;
-	
-	public HuffmanTree(){
+
+	public HuffmanTree() {
 		root = null;
 		current = null;
 	}
-	
+
 	/**
 	 * Makes a one node Huffman tree
-	 * @param d character to place in a single node tree
+	 * 
+	 * @param d
+	 *            character to place in a single node tree
 	 */
-	public HuffmanTree(char d){
-		
+	public HuffmanTree(char d) {
+		root = new Node(null, d, null);
+		current = root;
 	}
-	
+
 	/**
 	 * A tree where a node is either a leaf or has two children
-	 * @param t represents a post order traversal of the tree
-	 * @param nonLeaf is the char value of the data in the non-leaf nodes
+	 * 
+	 * @param t
+	 *            represents a post order traversal of the tree
+	 * @param nonLeaf
+	 *            is the char value of the data in the non-leaf nodes
 	 */
-	public HuffmanTree(String t, char nonLeaf){
-		
+	public HuffmanTree(String t, char nonLeaf) {
+
 	}
-	
+
 	/**
 	 * Merges b1 with b2
-	 * @param b1 Right leaf
-	 * @param b2 Left leaf
-	 * @param d value of the non-leaf data node to merge with
+	 * 
+	 * @param b1
+	 *            Right leaf
+	 * @param b2
+	 *            Left leaf
+	 * @param d
+	 *            value of the non-leaf data node to merge with
 	 */
-	public HuffmanTree(HuffmanTree b1, HuffmanTree b2, char d){
-		
+	public HuffmanTree(HuffmanTree b1, HuffmanTree b2, char d) {
+		root = new Node(b1.root, d, b2.root);
+		current = root;
 	}
-	
-	/* The following methods allow a user object to follow a path in the tree.
+
+	/*
+	 * The following methods allow a user object to follow a path in the tree.
 	 * Each method except atLeaf and current changes the value of current
 	 */
-	
-	public void moveRoot(){
-		
+
+	public void moveRoot() {
+
 	}
-	
-	public void moveLeft(){
-		
+
+	public void moveLeft() {
+
 	}
-	
-	public void moveRight(){
-		
+
+	public void moveRight() {
+
 	}
-	
+
 	/**
-	 * atLeaf returns true if the current position is a leaf, 
-	 * otherwise it returns false
+	 * atLeaf returns true if the current position is a leaf, otherwise it
+	 * returns false
+	 * 
 	 * @return atLeaf boolean operation
 	 */
-	public boolean atLeaf(){
-		if(current.right == null && current.left == null){
+	public boolean atLeaf() {
+		if (current.right == null && current.left == null) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * current returns the data value in the current Node
+	 * 
 	 * @return current.data
 	 */
-	public char current(){
+	public char current() {
 		return current.data;
 	}
-	
-	public class PathIterator implements Iterator<String>{
-		public PathIterator(){
-			
+
+	public class PathIterator implements Iterator<String> {
+		private LinkedList<String> paths;
+
+		public PathIterator() {
+			paths = new LinkedList<>();
+			makePath(root, "");
 		}
-		
-		public boolean hasNext(){
-			return false;
-			
+
+		private void makePath(Node r, String path) {
+			if (atLeaf()) {
+				paths.add(r.data + path);
+			} else {
+				makePath(r.left, path + "0");
+				makePath(r.right, path + "1");
+			}
 		}
-		
-		public String next(){
-			return null;
-			
+
+		public boolean hasNext() {
+			return paths.size() < 1;
+		}
+
+		public String next() {
+			return paths.poll();
 		}
 	}
-	
-	public Iterator<String> iterator(){
-		return null;
-		
+
+	public Iterator<String> iterator() {
+		return new PathIterator();
+
 	}
-	
+
 	/**
-	 * Returns a post order representation of the tree
-	 * in the format we discussed in class
+	 * Returns a post order representation of the tree in the format we
+	 * discussed in class
 	 */
-	public String toString(){
-		return null;
-		
+	public String toString() {
+		return toString(root);
+	}
+	
+	private String toString(Node r){
+		if(r == null){
+			return "";
+		}else {
+			return toString(r.left) + toString(r.right) + r.data;
+		}
 	}
 }
-
-
