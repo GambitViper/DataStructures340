2d460df58a0eb65785d589f998f8c9c3bce6c360
GambitViper <baklund.zachary@uwlax.edu>
2017-10-23 16:23:23 -0500

    Added comments for the Decode java class

--- HuffmanCoding/src/HuffmanDecode.java
+++ HuffmanCoding/src/HuffmanDecode.java
@@ -12,13 +12,23 @@ public class HuffmanDecode {
 	 */
 	public HuffmanDecode(String in, String out) {
 		HuffmanInputStream decode = new HuffmanInputStream(in);
+		//Gets the post-order representation of the tree from the input stream
 		String treeToBuild = decode.getTree();
+		//Stores the total number of characters it has to look to construct from the binary / compressed file
 		totalChars = decode.totalChars();
+		//Rebuilds the tree using the post-order representation
 		HuffmanTree rebuilt = new HuffmanTree(treeToBuild, (char) 128);
+		//Takes the input stream, the rebuilt representation and the desired destination to decompress to
 		decodeOut(decode, rebuilt, out);
 
 	}
 
+	/**
+	 * Work horse of the decode program, this decodes a compressed file to a desired location
+	 * @param stream - input stream used to read bits at a time
+	 * @param tree - rebuilt tree used to traverse and find character encodings
+	 * @param outFile - desired output location for the new decompressed file
+	 */
 	private void decodeOut(HuffmanInputStream stream, HuffmanTree tree, String outFile) {
 		try {
 			PrintWriter fout = new PrintWriter(outFile);
