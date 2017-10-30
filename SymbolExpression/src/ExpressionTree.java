8fed90bb20be4018e0cdeeb8fa08c6a8819d2e50
GambitViper <baklund.zachary@uwlax.edu>
2017-10-30 11:16:57 -0500

    Updated method stubs

--- SymbolExpression/src/ExpressionTree.java
+++ SymbolExpression/src/ExpressionTree.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 /**
  * 
  */
@@ -8,12 +10,63 @@
  */
 public class ExpressionTree {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private class Node {
+		private Node left;
+		private String data;
+		private Node right;
 
+		private Node(Node l, String d, Node r) {
+			left = l;
+			data = d;
+			right = r;
+		}
+	}
+
+	private Node root;
+
+	public ExpressionTree(String exp) {
+		// PRE: exp is a legal infix expression
+		// Build an expression tree from the expression exp
+
+	}
+
+	public int evaluate(SymbolTable t) {
+		// return the int value of the expression tree 
+		//t is used to lookup values of variables 
+		return evaluate(t, root);
+	}
+
+	private int evaluate(SymbolTable t, Node r) {
+		// return the int value of the expression tree with root r
+		// t is used to lookup values of variables
+		return -1;
+	}
+
+	public String toPostfix() {
+		// return the postfix representation of the expression tree
+		return toPostfix(root);
+	}
+
+	private String toPostfix(Node r) {
+		// return the postfix representation of the tree with root r
+		return null;
+	}
+
+	public String toInfix() {
+		// return the fully parenthesized infix
+		// representation of the expression tree
+
+		return toInfix(root);
+	}
+
+	private String toInfix(Node r) {
+		// return the fully parenthesized infix representation of the tree with
+		// root r
+		return null;
+	}
+	
+	public static void main(String args[]) throws IOException {
+		//used to test expression tree
 	}
 
 }
--- SymbolExpression/src/SymbolTable.java
+++ SymbolExpression/src/SymbolTable.java
@@ -11,61 +11,105 @@ import java.util.*;
 public class SymbolTable {
 
 	/**
-	 * A node used to build linked lists
-	 * not utilizing the Java LinkedList class
+	 * A node used to build linked lists not utilizing the Java LinkedList class
 	 */
-	private class Node{
-		
+	private class Node {
+
 		private String key;
 		private Object data;
 		private Node next;
-		
-		private Node(String k, Object d, Node x){
+
+		private Node(String k, Object d, Node x) {
 			key = k;
 			data = d;
 			next = x;
 		}
 	}
-	
+
 	private Node table[];
-	
+
 	/**
-	 * @param s is the size of the table. Does not handle resizing the table
+	 * @param s
+	 *            is the size of the table. Does not handle resizing the table
 	 */
-	public SymbolTable(int s){
-		
+	public SymbolTable(int s) {
+
 	}
-	
+
 	/**
-	 * @param k - key
+	 * @param k
+	 *            - key
 	 * @return the hash function value for k
 	 */
-	private int hash(String k){
+	private int hash(String k) {
 		return -1;
 	}
-	
-	public boolean insert(String k){
-	//if k is not in the table create a new entry (with a null data value)
-	//and return true if k is in the table return false
+
+	public boolean insert(String k) {
+		// if k is not in the table create a new entry (with a null data value)
+		// and return true if k is in the table return false
 		return false;
 	}
-	
-	public	boolean	find(String	k)	{	
-	//return	true	if	k	is	in	the	table	otherwise	return	false	
+
+	public boolean find(String k) {
+		// return true if k is in the table otherwise return false
 		return false;
-	}	
-	public	Object	getData(String	k)	{	
-	//if	k	is	in	the	table	return	the	data	(which	could	be	null)	associated	with	k	
-	//if	k	is	not	in	the	table	return	null	
+	}
+
+	public Object getData(String k) {
+		// if k is in the table return the data (which could be null) associated
+		// with k
+		// if k is not in the table return null
 		return null;
-	}	
-	public	void	setValue(String	k,	Object	d)	{	
-	//PRE:	k	is	in	the	table	
-	//make	d	the	data	value	associated	with	k		
-	}	
-	
+	}
+
+	public void setValue(String k, Object d) {
+		// PRE: k is in the table
+		// make d the data value associated with k
+	}
+
+	public class STIterator implements Iterator<String> {
+		// An iterator that
+		// iterates through
+		// the keys in the
+		// table
+
+		public STIterator() {
+
+		}
+
+		public boolean hasNext() {
+			return false;
+		}
+
+		public String next() {
+			// PRE: hasNext() //The format of the string
+			// should be key:data where key is a key in the
+			// //symbol table and data is the string
+			// representaGon of the data associated //with
+			// the key
+			return null;
+		}
+
+		public void remove() { // optional method not implemented
+
+		}
+	}
+
+	public boolean remove(String k) {
+		// if k is in the table, return the entry
+		// for k and return true //if k is not
+		// in the table, return false
+		return false;
+	}
+
+	public Iterator<String> iterator() {
+		// return a new STIterator object
+		return new STIterator();
+	}
+
 	public static void main(String[] args) {
-		//code to test SymbolTable
+		// code to test SymbolTable
 
 	}
 
