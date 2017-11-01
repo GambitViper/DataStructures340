da48552b4103502aed99db5c1628347f2e2e4840
GambitViper <baklund.zachary@uwlax.edu>
2017-11-01 14:55:41 -0500

    Initial development in expression tree

--- SymbolExpression/src/ExpressionTree.java
+++ SymbolExpression/src/ExpressionTree.java
@@ -1,4 +1,5 @@
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 /**
  * 
@@ -27,7 +28,35 @@ public class ExpressionTree {
 	public ExpressionTree(String exp) {
 		// PRE: exp is a legal infix expression
 		// Build an expression tree from the expression exp
+		Stack<String> operators = new Stack<>();
+		Stack<String> operands = new Stack<>();
+		String[] tokens = exp.trim().split(" ");
+		for(int i = 0; i < tokens.length; i++){
+			if(isOperand(tokens[i].charAt(0))){
+				operands.push(tokens[i]);
+			}else {
+				operators.push(tokens[i]);
+			}
+		}
+		System.out.println("Operators: " + operators);
+		System.out.println("Operands : " + operands);
+		
+		
 
+	}
+	
+	private boolean isOperand(char o){
+		return precedence(o) == 0;
+	}
+	
+	private int precedence(char c){
+		if(c == '+'|| c == '-') return 1;
+		if(c == '*' || c == '/' || c == '%') return 2;
+		if(c == '^') return 3;
+		if(c == '!') return 4;
+		if(c == '(') return 4;
+		if(c == ')') return -1;
+		return 0;
 	}
 
 	public int evaluate(SymbolTable t) {
@@ -52,8 +81,10 @@ public class ExpressionTree {
 
 	private String toPostfix(Node r) {
 		// return the postfix representation of the tree with root r
-		
-		return null;
+		if (r == null){
+			return "";
+		}
+		return toPostfix(r.left) + toPostfix(r.right) + r.data;
 	}
 
 	public String toInfix() {
@@ -66,12 +97,16 @@ public class ExpressionTree {
 	private String toInfix(Node r) {
 		// return the fully parenthesized infix representation of the tree with
 		// root r
-		
-		return null;
+		if (r == null){
+			return "";
+		}
+		return "(" + toInfix(r.left) + r.data + toInfix(r.right) + ")";
 	}
 	
 	public static void main(String args[]) throws IOException {
 		//used to test expression tree
+		String exp = "A + B * C ^ ( D - E ) ^ F + H / I";
+		ExpressionTree tree = new ExpressionTree(exp);
 	}
 
 }
