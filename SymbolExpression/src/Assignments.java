6e5be222de9df61f0981490908f235defa5a2a93
GambitViper <baklund.zachary@uwlax.edu>
2017-11-05 17:45:28 -0600

    Added assignments class

--- SymbolExpression/src/Assignments.java
+++ SymbolExpression/src/Assignments.java
@@ -6,14 +6,31 @@
  * @author Zach
  *
  */
+import java.io.*;
+
 public class Assignments {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader fin = new BufferedReader(new FileReader(args[0]));
+        SymbolTable symbolTable = new SymbolTable(10);
+        String line = "";
+        
+        //Loop through the lines
+        while ((line = fin.readLine()) != null) {
+            String[] expression = line.split(" = ");
+            symbolTable.insert(expression[0].trim());
+            //Convert right side of '=' to expression tree
+            ExpressionTree expTree = new ExpressionTree(expression[1].trim());
+            //Evaluate and add as value for the assigning key
+            int evaluation = expTree.evaluate(symbolTable);
+            symbolTable.setValue(expression[0], evaluation);
+        }
+        
+        //Output results
+        for (String string : symbolTable) {
+            System.out.println(string);
+        }
+        
+        fin.close();
+    }
 }
