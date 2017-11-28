49196d4facf9a92d2b21cae76fe5b92ca902b3aa
GambitViper <baklund.zachary@uwlax.edu>
2017-11-27 21:46:21 -0600

    "finished"

--- B+Tree/src/BTree.java
+++ B+Tree/src/BTree.java
@@ -7,84 +7,528 @@ import java.io.*;
 import java.util.*;
 
 public class BTree {
-	
+
 	RandomAccessFile f;
 	int order;
 	int blockSize;
 	long root;
 	long free;
-	//add instance variables as needed.
-	
-	private class BTreeNode{
+	Stack<BTreeNode> paths;
+	int mid;
+	// add instance variables as needed.
+
+	private class BTreeNode {
 		private int count;
 		private int keys[];
 		private long children[];
-		
-		//constructors and other method
+		private long addr;
+
+		// constructors and other method
+		private BTreeNode(int count, int keys[], long children[], long addr) {
+			this.count = count;
+			this.keys = keys;
+			this.children = children;
+			this.addr = addr;
+		}
+
+		private BTreeNode(long addr) throws IOException {
+			if (addr != 0) {
+				// Allocate space for the keys and children
+				this.keys = new int[order - 1];
+				this.children = new long[order];
+				// Seek to the memory address addr
+				f.seek(addr);
+				// Set the internal address property and read the count
+				this.addr = addr;
+				this.count = f.readInt();
+				// Loop to read the keys in the node
+				for (int i = 0; i < order - 1; i++) {
+					keys[i] = f.readInt();
+				}
+				// Loop to read the children node pointers
+				for (int j = 0; j < order; j++) {
+					children[j] = f.readLong();
+				}
+			}
+		}
+
+		private void writeBTreeNode() throws IOException {
+			f.seek(addr);
+
+			f.writeInt(count);
+			// Write the keys
+			for (int i = 0; i < order - 1; i++) {
+				f.writeInt(keys[i]);
+			}
+
+			// Write the child addresses
+			for (int j = 0; j < order; j++) {
+				f.writeLong(children[j]);
+			}
+		}
+
+		private boolean isLeaf() {
+			return this.count < 0;
+		}
+
+		private boolean notFull() {
+			return Math.abs(count) < (order - 1);
+		}
+
+		private void insertNode(int k, long addr) {
+			if (isLeaf()) {
+				// Add to the count
+				count--;
+				// n will be inserted count
+				int n = Math.abs(count);
+
+				// New key added at end of the keys
+				keys[n - 1] = k;
+				// Put address at the second to last place in children
+				children[n - 1] = addr;
+				
+				//Calculate max
+				int max = 0;
+				if(keys[n - 2] > k){
+					max = keys[n - 2];
+				}else {
+					max = k;
+				}
+				
+				//Intermediate save array
+				long[] intermediate = new long[++max];
+				
+				// Is leaf possibly new smallest key
+				for (int i = 0; i < n; i++) {
+					intermediate[keys[i]] = children[i];
+				}
+				
+				for (int i = 1; i < n; ++i) {
+					int key = keys[i];
+					int j = i - 1;
+
+					// Shift values in array
+					while (j >= 0 && keys[j] > key) {
+						keys[j + 1] = keys[j];
+						j--;
+					}
+
+					keys[j + 1] = key;
+				}
+				
+				// Save an in-between table of all the associated children addresses
+				for (int i = 0; i < n; i++) {
+					children[i] = intermediate[keys[i]];
+				}
+
+			} else {//Non-Leaf node
+				// Add to the count
+				count++;
+				// n will be inserted count
+				int n = Math.abs(count);
+
+				// New key added at end of the keys
+				keys[n - 1] = k;
+				// If not a leaf, possibly overwrote the last value
+				children[n] = addr;
+				
+				//Calculate the max
+				int max = 0;
+				if(keys[n - 2] > k){
+					max = keys[n - 2];
+				}else {
+					max = k;
+				}
+				
+				//intermediate save array
+				long[] intermediate = new long[++max];
+				
+				// Not a leaf no change to children[0]
+				for (int i = 0; i < n; i++) {
+					intermediate[keys[i]] = children[i + 1];
+				}
+				
+				for (int i = 1; i < n; ++i) {
+					int key = keys[i];
+					int j = i - 1;
+
+					// Shift values in array
+					while (j >= 0 && keys[j] > key) {
+						keys[j + 1] = keys[j];
+						j--;
+					}
+
+					keys[j + 1] = key;
+				}
+				
+				// Children array to match the key array
+				for (int i = 0; i < n; i++) {
+					children[i + 1] = intermediate[keys[i]];
+				}
+			}
+		}
+
+		private BTreeNode split(int key, long addr) {
+			//Copy arrays with one more space
+			int[] keyCopyOneMore = Arrays.copyOf(keys, order);
+			long[] childrenCopyOneMore = Arrays.copyOf(children, order + 1);
+			
+			//New node with 1 extra space for the insert
+			BTreeNode splitNode = new BTreeNode(count, keyCopyOneMore, childrenCopyOneMore,this.addr);
+
+			if (key != -1 && addr != -1) {
+				//Add the new inserted node into the tree
+				splitNode.insertNode(key, addr);
+			}
+			
+			//Calculation for the new count of the node
+			int newCount = (int) Math.ceil((double) splitNode.keys.length / 2);
+			
+			//Split the values between node and the new node and update old node's count
+			int oldNodeNewCount = Math.abs(count) + 1 - newCount;
+			if (isLeaf()) {
+				count = oldNodeNewCount * -1;
+			} else {
+				count = oldNodeNewCount;
+			}
+
+			// Link to next node to copy into the last child address to keep link to next node
+			long nextLeaf = children[order - 1];
+
+			// Get the new values for the old node
+			int[] origKeys = Arrays.copyOfRange(splitNode.keys, 0, Math.abs(count));
+			long[] origChilds = Arrays.copyOfRange(splitNode.children, 0, Math.abs(count) + 1);
+			
+			//Copy into original nodes keys and children node properties
+			keys = Arrays.copyOf(origKeys, order - 1);
+			children = Arrays.copyOf(origChilds, order);
+
+			// Get the new values for the new node. Starting at the middle until the end.
+			int[] splitKeys = Arrays.copyOfRange(splitNode.keys, newCount - 1, splitNode.keys.length);
+			long[] splitChilds = Arrays.copyOfRange(splitNode.children, newCount - 1, splitNode.children.length);
+			
+			//Copy into
+			int[] keyArr = Arrays.copyOf(splitKeys, order - 1);
+			long[] childrenArr = Arrays.copyOf(splitChilds, order);
+
+			if (order % 2 == 0 || !splitNode.isLeaf()) {
+				//Set the middle value for later loops
+				mid = keyArr[0];
+				
+				// This will copy the array starting 1 place over to the right
+				splitKeys = Arrays.copyOfRange(splitNode.keys, newCount, splitNode.keys.length);
+				splitChilds = Arrays.copyOfRange(splitNode.children, newCount, splitNode.children.length);
+				
+				//Copy into
+				keyArr = Arrays.copyOf(splitKeys, order - 1);
+				childrenArr = Arrays.copyOf(splitChilds,order);
+				
+				//Odd order tree rectify
+				if (order % 2 == 1) {
+					newCount--;
+				}
+			}
+			
+			//Rectify negative count index
+			if (splitNode.isLeaf()) {
+				newCount *= -1;
+			}
+
+			//Keep listed link to the next node and add the new node
+			childrenArr[order - 1] = nextLeaf;
+			BTreeNode newnode = new BTreeNode(newCount, keyArr, childrenArr, findFree());
+			children[order - 1] = newnode.addr;
+
+			return newnode;
+		}
 	}
-	
-	public BTree(String filename, int bsize){
-		//bsize is the block size. This value is used to calculate the order of the B+Tree
-		//all B+Tree nodes will use bsize bytes
-		//make a new B+tree
+
+	public BTree(String filename, int bsize) throws IOException {
+		// bsize is the block size. This value is used to calculate the order of
+		// the B+Tree
+		// all B+Tree nodes will use bsize bytes
+		// make a new B+tree
+		File path = new File(filename);
+		if (path.exists()) {
+			path.delete();
+		}
+		f = new RandomAccessFile(path, "rw");
+
+		root = 0;
+		free = 0;
+		blockSize = bsize;
+		order = bsize / 12;
+		paths = new Stack<>();
+		// Write initialized BTree
+		f.seek(0);
+		f.writeLong(root);
+		f.writeLong(free);
+		f.writeInt(blockSize);
+
 	}
-	
-	public BTree(String filename){
-		//open an existing B+Tree
+
+	public BTree(String filename) throws IOException {
+		// open an existing B+Tree
+		File path = new File(filename);
+		f = new RandomAccessFile(path, "rw");
+		f.seek(0);
+		root = f.readLong();
+		free = f.readLong();
+		blockSize = f.readInt();
+		order = blockSize / 12;
+		paths = new Stack<>();
 	}
-	
-	public boolean insert(int key, long addr){
+
+	public boolean insert(int key, long addr) throws IOException {
 		/*
-		 * If key is not a duplicate add, key to the B+tree
-		 * addr (in DBTable) is the address of the row that contains the key
-		 * return true if the key is added
-		 * return false if the key is a duplicate
+		 * If key is not a duplicate add, key to the B+tree addr (in DBTable) is
+		 * the address of the row that contains the key return true if the key
+		 * is added return false if the key is a duplicate
 		 */
-		return false;
+		if (root == 0) {
+			int[] keys = new int[order - 1];
+			long[] children = new long[order];
+			keys[0] = key;
+			children[0] = addr;
+			BTreeNode r = new BTreeNode(-1, keys, children, findFree());
+
+			r.writeBTreeNode();
+
+			root = r.addr;
+			return true;
+		}
+		boolean alreadyPresent = 0 != search(key);
+		boolean split = false;
+		if (!alreadyPresent) {
+			long location = 0;
+			int value = 0;
+			// Perform Insertion
+			BTreeNode r = paths.pop();
+			//Node r has room to insert the key
+			if (r.notFull()) {
+				//Perform the insert and write the new node out to the random Access file
+				r.insertNode(key, addr);
+				r.writeBTreeNode();
+			} else {
+				//Node r is full , which requires a split
+				BTreeNode newNode = r.split(key, addr);
+				//Keep track of the value to update parent node's keys
+				value = newNode.keys[0];
+				//Write the nodes out
+				r.writeBTreeNode();
+				newNode.writeBTreeNode();
+				//Update the new node's location
+				location = newNode.addr;
+				split = true;
+			}
+			
+			//Paths aren't empty and you have split
+			while (!paths.empty() && split) {
+				//Node is the next node on the path's stack
+				BTreeNode node = paths.pop();
+				if (node.notFull()) {
+					//Node node is not full so you can insert as normal
+					node.insertNode(value, location);
+					//Write the node out and change the loop's condition
+					node.writeBTreeNode();
+					split = false;
+				} else {
+					//Node node was full so you have to perform a split
+					BTreeNode newNode = node.split(value, location);
+					//Update the value and location
+					value = mid;
+					location = newNode.addr;
+					
+					//Write the nodes out and keep loop condition to update further up in the path's stack
+					node.writeBTreeNode();
+					newNode.writeBTreeNode();
+					
+					//Continue the loop condition
+					split = true;
+				}
+			}
+			
+			//Case for the split root
+			if (split) {
+				//Setup space for the keys and children values
+				int[] keys = new int[order - 1];
+				long[] children = new long[order];
+				//Utilize the value and location data for the parent of the root
+				keys[0] = value;
+				children[0] = root;
+				children[1] = location;
+				
+				//Find a new location for the split of the root node
+				BTreeNode newNode = new BTreeNode(1, keys, children, findFree());
+				root = newNode.addr;
+
+				//Write the node out
+				newNode.writeBTreeNode();
+			}
+		}
+		
+		//Return the boolean value of whether or not the given insert was performed
+		return !alreadyPresent;
+
+	}	
+
+	private long findFree() {
+		//Find new location to insert new node
+		long addr = 0;
+		try {
+			//If free is empty return the length
+			if (free == 0) {
+				addr = f.length();
+			} else {
+				//Retain linked list in the free list
+				addr = free;
+				f.seek(free);
+
+				free = f.readLong();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//Return free address
+		return addr;
 	}
-	
-	public long remove(int key){
+
+	public long remove(int key) {
 		/*
-		 * If the key is in the Btree, remove the key and return the address of the row
-		 * return 0 if the key is not found in the B+tree
+		 * If the key is in the Btree, remove the key and return the address of
+		 * the row return 0 if the key is not found in the B+tree
 		 */
 		return key;
 	}
-	
-	public long search(int k){
+
+	public long search(int k) throws IOException {
 		/*
 		 * This is an equality search
 		 * 
 		 * If the key is found return the address of the row with the key
 		 * otherwise return 0
 		 */
-		return 0;
+
+		//Create a new path's stack
+		int i = 0;
+		paths = new Stack<>();
+		//Start at the root
+		BTreeNode r = new BTreeNode(root);
+		paths.push(r);
+		if (root == 0) {
+			return 0;
+		}
+
+		//Get to a leaf node
+		while (!r.isLeaf()) {
+			for (i = 0; i <= Math.abs(r.count); i++) {
+				if (i == Math.abs(r.count) || k < r.keys[i]) {
+					r = new BTreeNode(r.children[i]);
+					paths.push(r);
+					break;
+				}
+			}
+		}
+		
+		//Finding the address of the key equality search
+		long addr = 0;
+		int idx = -1;
+		for (int j = 0; j < Math.abs(r.count); j++) {
+			if (k == r.keys[j]) {
+				idx = i;
+			}
+		}
+
+		if (idx != -1) {
+			addr = r.children[idx];
+		}
+
+		return addr;
 	}
-	
-	public LinkedList<Long> rangeSearch(int low, int high){
-		//PRE:low <= high
+
+	public LinkedList<Long> rangeSearch(int low, int high) throws IOException {
+		// PRE:low <= high
 		/*
-		 * return a list of row addresses for all keys in the range low to high inclusive
-		 * return an empty list when no keys are in the range
+		 * return a list of row addresses for all keys in the range low to high
+		 * inclusive return an empty list when no keys are in the range
 		 */
-		return null;
-	}
-	
-	public void print(){
-		//print the B+Tree to standard output
-		//print one node per line
-	}
-	
-	public void close(){
-		//close the B+tree. The tree should not be accessed after close is called
+		LinkedList<Long> list = new LinkedList<>();
+		int i = 0;
+		paths = new Stack<>();
+		BTreeNode r = new BTreeNode(root);
+		BTreeNode look = null;
+		paths.push(r);
+		if (root == 0) {
+			look = r;
+		}
+
+		while (!r.isLeaf()) {
+			for (i = 0; i < Math.abs(r.count); i++) {
+				if (i == Math.abs(r.count) || low < r.keys[i]) {
+					r = new BTreeNode(r.children[i]);
+					paths.push(r);
+					break;
+				}
+			}
+		}
+
+		if (look == null) {
+			look = r;
+		}
+
+		while (low < look.keys[i] && look.keys[i] < high) {
+			list.add(look.children[i]);
+			i++;
+			if (i == Math.abs(look.count)) {
+				if (look.children[order - 1] == 0) {
+					// End of linked list of leaf nodes
+					break;
+				}
+				// Look at next leaf in the linked list of leaf nodes
+				look = new BTreeNode(look.children[order - 1]);
+				i = 0;
+			}
+		}
+
+		return list;
 	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void print() throws IOException {
+		// print the B+Tree to standard output
+		// print one node per line
+		System.out.printf("%10s: \n", "Address");
+		System.out.printf("%10s: %2d\n", "Root", root);
+		System.out.printf("%10s: %2d\n", "Free", free);
+		f.seek(20);
+		while(f.getFilePointer() < f.length()){
+			printAddr(f.getFilePointer());
+		}
+	}
 
+	private void printAddr(long addr) throws IOException {
+		f.seek(addr);
+		BTreeNode curr = new BTreeNode(addr);
+		String addrStr = Long.toString(curr.addr);
+		String countStr = " " + curr.count + " ";
+		String keyStr = "";
+		for (int key : curr.keys) {
+			keyStr += key + " ";
+		}
+		String childStr = "";
+		for (long child : curr.children) {
+			childStr += child + " ";
+		}
+		System.out.printf("%10s: Count: %4s | Keys: %10s | Children: %4s\n", addrStr, countStr, keyStr, childStr);
+	}
+
+	public void close() throws IOException {
+		// close the B+tree. The tree should not be accessed after close is
+		// called
+		f.seek(0);
+		f.writeLong(root);
+		f.writeLong(free);
+		f.writeInt(blockSize);
+		f.close();
 	}
 
 }
--- B+Tree/src/DBTable.java
+++ B+Tree/src/DBTable.java
@@ -2,117 +2,238 @@ import java.io.*;
 import java.util.*;
 
 public class DBTable {
-	
-	//The file that stores the row in the table
+
+	// The file that stores the row in the table
 	RandomAccessFile rows;
-	//Head of the free list space for rows
+	// Head of the free list space for rows
 	long free;
 	int numOtherFields;
 	int otherFieldLengths[];
-	//Add other instance variables as needed
-	
+	// Add other instance variables as needed
+	BTree tree;
+
 	private class Row {
 		private int keyField;
 		private char otherFields[][];
+		private long addr;
+
 		/*
-		 * Each row consists of unique key and one or more character array fields.
+		 * Each row consists of unique key and one or more character array
+		 * fields.
 		 * 
-		 * Each character array field is a fixed length field (for example 10 characters).
+		 * Each character array field is a fixed length field (for example 10
+		 * characters).
 		 * 
 		 * Each field can have a different length
 		 * 
-		 * Fields are padded with null characters so a field with a length of
-		 * x characters always uses space for x character
+		 * Fields are padded with null characters so a field with a length of x
+		 * characters always uses space for x character
 		 */
-		
-		//Constructors and other Row methods
+		private Row(int keyField, char otherFields[][], long addr) {
+			this.keyField = keyField;
+			this.otherFields = otherFields;
+			this.addr = addr;
+		}
+
+		private Row(long addr) throws IOException {
+			rows.seek(addr);
+			this.addr = addr;
+			this.keyField = rows.readInt();
+			this.otherFields = new char[numOtherFields][];
+			for (int i = 0; i < numOtherFields; i++) {
+				otherFields[i] = new char[otherFieldLengths[i]];
+				for (int j = 0; j < otherFields[i].length; j++) {
+					otherFields[i][j] = rows.readChar();
+				}
+			}
+		}
+
+		private void writeRow() throws IOException {
+			rows.seek(addr);
+			rows.writeInt(keyField);
+			for (int i = 0; i < numOtherFields; i++) {
+				for (int j = 0; j < otherFieldLengths[i]; j++) {
+					rows.writeChar(otherFields[i][j]);
+				}
+			}
+		}
+
+		// Constructors and other Row methods
 	}
-	
-	public DBTable(String filename, int fl[], int bsize){
+
+	public DBTable(String filename, int fl[], int bsize) throws IOException {
 		/*
 		 * Use this constructor to create a new DBTable.
 		 * 
-		 * filename is the name of the file used to store the table
-		 * fl is the lengths of the otherFields
-		 * fl.length indicates how many other fields are part of the row
-		 * bsize is the block size. It is used to calculate the order of the B+Tree
+		 * filename is the name of the file used to store the table fl is the
+		 * lengths of the otherFields fl.length indicates how many other fields
+		 * are part of the row bsize is the block size. It is used to calculate
+		 * the order of the B+Tree
 		 * 
 		 * A B+Tree must be created for the key field in the table
 		 * 
-		 * If a file with name filename exists, the file should be deleted before the
-		 * new file is created.
+		 * If a file with name filename exists, the file should be deleted
+		 * before the new file is created.
 		 */
+		File path = new File(filename);
+		File treePath = new File(filename + "_tree");
+		if (path.exists()) {
+			path.delete();
+			treePath.delete();
+		}
+		rows = new RandomAccessFile(path, "rw");
+		rows.seek(0);
+		numOtherFields = fl.length;
+
+		rows.writeInt(numOtherFields);
+		otherFieldLengths = new int[numOtherFields];
+		for (int i = 0; i < numOtherFields; i++) {
+			otherFieldLengths[i] = fl[i];
+			rows.writeInt(fl[i]);
+		}
+		tree = new BTree(filename + "_ofTree", bsize);
+		free = 0;
 	}
-	
-	public DBTable(String filename){
-		//Use this constructor to open an existing DBTable
+
+	public DBTable(String filename) throws IOException {
+		// Use this constructor to open an existing DBTable
+		rows = new RandomAccessFile(filename, "rw");
+		rows.seek(0);
+
+		numOtherFields = rows.readInt();
+		otherFieldLengths = new int[numOtherFields];
+
+		for (int i = 0; i < numOtherFields; i++) {
+			otherFieldLengths[i] = rows.readInt();
+		}
+		free = rows.readLong();
+		tree = new BTree(filename + "_ofTree");
 	}
-	
-	public boolean insert(int key, char fields[][]){
-		//PRE:the length of each row is fields matches the expected length
+
+	public boolean insert(int key, char fields[][]) throws IOException {
+		// PRE:the length of each row is fields matches the expected length
 		/*
-		 * If a row with the key is not in the table, the row is added and the method
-		 * returns true otherwise the row is not added and the method returns false.
+		 * If a row with the key is not in the table, the row is added and the
+		 * method returns true otherwise the row is not added and the method
+		 * returns false.
 		 * 
-		 * The method must use the B+tree to determine if a row with the key exists.
+		 * The method must use the B+tree to determine if a row with the key
+		 * exists.
 		 * 
 		 * If the row is added the key is also added into the B+tree
 		 */
-		return false;
+		if (tree.search(key) != 0) {
+			return false;
+		}
+
+		long newAddr = findFree();
+		tree.insert(key, newAddr);
+
+		Row r = new Row(key, fields, newAddr);
+		r.writeRow();
+
+		return true;
 	}
-	
-	public boolean remove(int key){
+
+	public boolean remove(int key) throws IOException {
 		/*
-		 * If a row with the key is in the table it is removed and true is returned
-		 * otherwise false is returned
+		 * If a row with the key is in the table it is removed and true is
+		 * returned otherwise false is returned
 		 * 
-		 * The method must use the B+Tree to determine if a row with the key exists.
+		 * The method must use the B+Tree to determine if a row with the key
+		 * exists.
 		 * 
 		 * If the row is deleted the key must be deleted from the B+Tree
 		 */
-		return false;
+
+		return true;
 	}
-	
-	public LinkedList<String> search(int key){
+
+	public LinkedList<String> search(int key) throws IOException {
 		/*
-		 * If a row with the key is found in the table return a list of the other fields in the row.
-		 * The string values in the list should not include the null characters.
+		 * If a row with the key is found in the table return a list of the
+		 * other fields in the row. The string values in the list should not
+		 * include the null characters.
 		 * 
 		 * If a row with the key is not found return an empty list
 		 * 
 		 * The method must use the equality search in B+Tree
 		 */
-		return null;
+		long addr = tree.search(key);
+		if (addr == 0) {
+			return new LinkedList<String>();
+		}
+
+		LinkedList<String> list = new LinkedList<>();
+		Row r = new Row(addr);
+		String str = Integer.toString(r.keyField);
+		list.add(str);
+		for (int i = 0; i < numOtherFields; i++) {
+			str = "";
+			for (int j = 0; j < otherFieldLengths[i] && r.otherFields[i][j] != '\0'; j++) {
+				str += r.otherFields[i][j];
+			}
+			list.add(str);
+		}
+		
+		return list;
 	}
-	
-	public LinkedList<LinkedList<String>> rangeSearch(int low, int high){
-		//PRE:low <= high
+
+	public LinkedList<LinkedList<String>> rangeSearch(int low, int high) throws IOException {
+		// PRE:low <= high
 		/*
-		 * For each row with a key that is in the range low to high inclusive a list
-		 * of the fields in the row is added to the list returned by the call.
+		 * For each row with a key that is in the range low to high inclusive a
+		 * list of the fields in the row is added to the list returned by the
+		 * call.
 		 * 
 		 * If there are no rows with a key in the range return an empty list
 		 * 
 		 * This method must use the range search in B+Tree
 		 */
-		return null;
+		LinkedList<LinkedList<String>> list = new LinkedList<LinkedList<String>>();
+
+		for (long addr : tree.rangeSearch(low, high)) {
+			LinkedList<String> subList = new LinkedList<>();
+			Row r = new Row(addr);
+			String str = Integer.toString(r.keyField);
+			subList.add(str);
+			for (int i = 0; i < numOtherFields; i++) {
+				str = "";
+				for (int j = 0; j < otherFieldLengths[i] && r.otherFields[i][j] != '\0'; j++) {
+					str += r.otherFields[i][j];
+				}
+				subList.add(str);
+			}
+			list.add(subList);
+		}
+
+		return list;
 	}
 	
-	public void print(){
-		//Print the rows to standard output in ascending order (based on the keys)
-		//One row per line
-	}
-	
-	public void close(){
-		//close the DBTable. The table should not be used after it is closed
+	private long findFree() throws IOException{
+		long addr = 0;
+		if(free == 0){
+			addr = rows.length();
+		}else {
+			addr = free;
+			rows.seek(free);
+			free = rows.readLong();
+		}
+		return addr;
 	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void print() throws IOException {
+		// Print the rows to standard output in ascending order (based on the
+		// keys)
+		// One row per line
+		tree.print();
+	}
 
+	public void close() throws IOException {
+		// close the DBTable. The table should not be used after it is closed
+		rows.seek(4 * (numOtherFields + 1));
+		rows.writeLong(free);
+		tree.close();
 	}
 
 }
